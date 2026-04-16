062396672296e0550e3815bc2b849ec719c19576
raiden2990 <brayden2990@gmail.com>
2026-04-15 20:06:18 -0700

    more moolah

--- doorfornoobz-template-1.21.8/src/main/java/com/brayden/doorfornoobz/Moditems.java
+++ doorfornoobz-template-1.21.8/src/main/java/com/brayden/doorfornoobz/Moditems.java
@@ -13,4 +13,10 @@ public class Moditems {
 
     public static  final DeferredItem<SpawnEggItem> TOTALLY_nOT_AN_eGG_THATWILL_PROTECT_U = ITEMS.registerItem("totally_not_an_egg_thatwill_protect_u",
             properties -> new SpawnEggItem(ModEntityTypes.GUARDIAN_BOB.get(), properties));
+
+    public static final DeferredItem <Potatio_MINIGun> An_Item_That_Goes_Pew_Pew_Pew = ITEMS.registerItem("an_item_that_goes_pew_pew_pew", Potatio_MINIGun::new);
+
+    public static final DeferredItem <money_gun> bad_income = ITEMS.registerItem("bad_income", money_gun::new);
 }
+
+
---
+++ doorfornoobz-template-1.21.8/src/main/java/com/brayden/doorfornoobz/money_gun.java
@@ -0,0 +1,64 @@
+package com.brayden.doorfornoobz;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class money_gun extends Item {
+    HashMap <Item, Integer> RNG = new HashMap<>(){
+        {put(Items.IRON_INGOT,67);
+            put(Items.COAL,76);
+            put(Items.GOLD_INGOT,58);
+            put(Items.REDSTONE,36);
+            put(Items.EMERALD,24);
+            put(Items.DIAMOND,5);
+            put(Items.NETHERITE_SCRAP,1);}
+    };
+
+
+    public money_gun(Properties properties) {
+        super(properties);
+
+    }
+    @Override
+    public int getUseDuration(ItemStack stack, LivingEntity entity) {
+        return 72000;
+    }
+    @Override
+    public InteractionResult use(Level level, Player player, InteractionHand hand) {
+
+        player.startUsingItem(hand);
+        return InteractionResult.CONSUME;
+
+
+    }
+    @Override
+    public void onUseTick(Level level, LivingEntity livingEntity, ItemStack stack, int remainingUseDuration) {
+        if (!level.isClientSide&&getUseDuration(stack,livingEntity) -remainingUseDuration >20){
+        AtomicInteger sum = new AtomicInteger();
+        RNG.forEach((I,Wieght)->{
+
+            sum.addAndGet(Wieght);
+        });
+        int choice = level.random.nextInt(sum.get());
+        for(Map.Entry<Item,Integer>entry:RNG.entrySet()){
+            if (choice < entry.getValue()){
+                level.addFreshEntity(entry.getKey().createEntity(level,livingEntity,entry.getKey().getDefaultInstance()));
+                break;
+            }
+            else{
+                choice -= entry.getValue();
+            }
+        }
+        }
+}}
