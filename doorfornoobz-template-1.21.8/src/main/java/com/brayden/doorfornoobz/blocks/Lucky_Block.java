ca282945c8ebaf8c93b37180b18bf1fa1056e787
raiden2990 <brayden2990@gmail.com>
2026-05-16 12:02:18 -0700

    No Crashing

--- doorfornoobz-template-1.21.8/src/main/java/com/brayden/doorfornoobz/blocks/Lucky_Block.java
+++ doorfornoobz-template-1.21.8/src/main/java/com/brayden/doorfornoobz/blocks/Lucky_Block.java
@@ -16,16 +16,26 @@ public class Lucky_Block extends Block  {
     public Lucky_Block(Properties p_49795_) {
         super(p_49795_);
     }
-
     @Override
     public void destroy(LevelAccessor level, BlockPos pos, BlockState state) {
-        int choice = level.getRandom().nextInt(2);
+        int choice = level.getRandom().nextInt(3);
+
         if (choice==0){
             ItemStack item = new ItemStack(Moditems.bad_income.get(),1);
-            ItemEntity e = new ItemEntity(level.getServer().overworld(), pos.getX(),pos.getY(),pos.getZ(), item);
+            ItemEntity e = new ItemEntity((Level)level, pos.getX(),pos.getY(),pos.getZ(), item);
             level.addFreshEntity(e);
         }
-
+        else if (choice == 1) {
+            ItemStack item = new ItemStack(Moditems.TOTALLY_nOT_AN_eGG_THATWILL_PROTECT_U.get(), 1);
+            ItemEntity e = new ItemEntity((Level) level, pos.getX(), pos.getY(), pos.getZ(), item);
+            level.addFreshEntity(e);
+        }
+        else if (choice == 2) {
+            ItemStack item = new ItemStack(Moditems.An_Item_That_Goes_Pew_Pew_Pew.get(), 1);
+            ItemEntity e = new ItemEntity((Level) level, pos.getX(), pos.getY(), pos.getZ(), item);
+            level.addFreshEntity(e);
+        }
         super.destroy(level, pos, state);
     }
 }
+
